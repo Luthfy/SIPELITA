a76349a3c224cd5c623ec656cc96e7da66d597c4
Luthfy E. Permana <luthfy.megumi@gmail.com>
2020-04-15 17:43:09 +0800

    Change Base URL to BAPELKES

--- app/src/main/java/id/digilabyte/sipelita/constant/Base.java
+++ app/src/main/java/id/digilabyte/sipelita/constant/Base.java
@@ -1,5 +1,6 @@
 package id.digilabyte.sipelita.constant;
 
 public class Base {
-    public static final String DIGILABYTE_BASE_URL = "http://bapelkes.live.gahar.xyz/";
+//    public static final String DIGILABYTE_BASE_URL = "http://bapelkes.live.gahar.xyz/";
+    public static final String DIGILABYTE_BASE_URL = "http://sipelita.bapelkes.kalselprov.go.id/";
 }
