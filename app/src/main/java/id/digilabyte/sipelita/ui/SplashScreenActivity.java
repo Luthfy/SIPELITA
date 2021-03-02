5e3871ce449f84bebdbdc0594db9d9d957436aa0
Luthfy Eka Permana <luthfy.megumi@gmail.com>
2021-03-02 23:07:11 +0800

    update version text, android core, dan base url

--- app/src/main/java/id/digilabyte/sipelita/ui/SplashScreenActivity.java
+++ app/src/main/java/id/digilabyte/sipelita/ui/SplashScreenActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
+import id.digilabyte.sipelita.BuildConfig;
 import id.digilabyte.sipelita.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
@@ -14,6 +16,9 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        TextView txtVersion = findViewById(R.id.txt_version);
+        txtVersion.setText("version : " + BuildConfig.VERSION_NAME);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
